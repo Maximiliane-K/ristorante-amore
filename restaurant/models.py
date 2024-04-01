a4ef4ca05246fd3523adfbafd51afadaf1ae2de3
Maximiliane <maximiliane.kaempffer@gmail.com>
2024-04-01 21:05:43 +0000

    Add About model for content on landing page, along with corresponding admin integration

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class About(models.Model):
+    """
+    Model for About section on landing page
+    """
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'About'
+
+    def __str__(self):
+        return self.title
