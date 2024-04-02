0afdbfbd9bce22bfaaee492d37e706c5abaca222
Maximiliane <maximiliane.kaempffer@gmail.com>
2024-04-02 12:02:05 +0000

    Add host to ALLOWED_HOSTS in settings.py and add permission to prevent addition of new instances in Adbout admin after initial creation

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import About
 
 # Register your models here.
-admin.site.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        # Check if an instance of About already exists
+        about_instance = About.objects.exists()
+        # Allow adding if no instance exists, deny otherwise
+        return not about_instance
+
+admin.site.register(About, AboutAdmin)
+
+
 
