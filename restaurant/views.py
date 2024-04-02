039fc0f8086b17b35d763330ed1241bd9e353d98
Maximiliane <maximiliane.kaempffer@gmail.com>
2024-04-02 12:23:14 +0000

    Add view and template for displaying About content on landing page

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import About
 
 # Create your views here.
+def about_view(request):
+    """
+    View to display about content on landing page
+    """
+    about_content = About.objects.all().first()
+
+    context = {'about_content': about_content}
+
+    return render(request, 'index.html', context)
