727932bdcabfcc1bf4ceee93819a91e67ff451c4
Maximiliane <maximiliane.kaempffer@gmail.com>
2024-04-02 16:00:47 +0000

    Add view and template for displaying foodmenu items in food link

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import About
+from .models import About, Category, MenuItems
 
 # Create your views here.
 def about_view(request):
@@ -11,3 +11,14 @@ def about_view(request):
     context = {'about_content': about_content}
 
     return render(request, 'index.html', context)
+
+def foodmenu(request):
+    """
+    View to show all food menu items 
+    """
+    categories = Category.objects.all()
+    menu_items = MenuItems.objects.all()
+    
+    context = {'categories': categories, 'menu_items': menu_items}
+
+    return render(request, 'food.html', context)
