ebf8708185709b0af250f4aedee8907e792dfa69
Maximiliane <maximiliane.kaempffer@gmail.com>
2024-04-06 12:05:24 +0000

    Add view, template and form for displying the booking form and add table_booking URL to bookings app

---
+++ bookings/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Booking
+import datetime
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['guest_last_name', 'phone_number', 'date', 'time', 'num_people']
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'})
+        }
--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Booking
+from .forms import BookingForm
+from django.contrib import messages
 
 # Create your views here.
+def table_bookings(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.user = request.user  
+            booking.save()
+            return redirect()
+    else:
+        form = BookingForm()
+
+    return render(request, 'bookings/table_booking.html', {'form': form})
