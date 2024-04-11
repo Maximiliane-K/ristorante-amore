1e43f77e521d0e1eb3958217b06cd3103455a39c
Maximiliane <maximiliane.kaempffer@gmail.com>
2024-04-11 07:31:58 +0000

    Refactor indentation of restaurat models.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class About(models.Model):
     """
     Model for About section on landing page
@@ -14,9 +14,10 @@ class About(models.Model):
     def __str__(self):
         return self.title
 
+
 class Category(models.Model):
     """
-    Model for menu items categories 
+    Model for menu items categories
     """
     name = models.CharField(max_length=50)
 
@@ -26,19 +27,25 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class MenuItems(models.Model):
     """
     Model for the menu items
     """
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    category = models.ForeignKey(Category,related_name='menu_items', on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category,
+        related_name='menu_items',
+        on_delete=models.CASCADE
+    )
 
     class Meta:
         verbose_name_plural = 'Menu Items'
 
     def __str__(self):
         return self.name
+
 
 class BeverageCategory(models.Model):
     """
@@ -52,16 +59,22 @@ class BeverageCategory(models.Model):
     def __str__(self):
         return self.name
 
+
 class BeverageItems(models.Model):
     """
     Model for beverage items
     """
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    beverage_category = models.ForeignKey(BeverageCategory,related_name='beverage_items', on_delete=models.CASCADE)
+    beverage_category = models.ForeignKey(
+        BeverageCategory,
+        related_name='beverage_items',
+        on_delete=models.CASCADE
+    )
 
     class Meta:
         verbose_name_plural = 'Beverage Items'
 
     def __str__(self):
         return self.name
+        
