c5670c53248d5d2ef333fdf6319eb6ca4da4b7fd
Maximiliane <maximiliane.kaempffer@gmail.com>
2024-04-11 07:36:05 +0000

    Refactor indentation of restaurant views.py

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import About, Category, MenuItems, BeverageCategory, BeverageItems
 
-# Create your views here.
+
 def about_view(request):
     """
     View to display about content on landing page
@@ -12,31 +12,37 @@ def about_view(request):
 
     return render(request, 'index.html', context)
 
+
 def foodmenu(request):
     """
-    View to show all food menu items 
+    View to show all food menu items
     """
     categories = Category.objects.all()
     menu_items = MenuItems.objects.all()
-    
+
     context = {'categories': categories, 'menu_items': menu_items}
 
     return render(request, 'food.html', context)
 
+
 def beveragemenu(request):
     """
-    View to show all beverages available 
+    View to show all beverages available
     """
     beverage_categories = BeverageCategory.objects.all()
     beverage_items = BeverageItems.objects.all()
-    
-    context = {'beverage_categories': beverage_categories, 'beverage_items': beverage_items}
+
+    context = {
+        'beverage_categories': beverage_categories,
+        'beverage_items': beverage_items
+    }
 
     return render(request, 'drinks.html', context)
 
 
 def contact(request):
     """
-    View for displaying contact page 
+    View for displaying contact page
     """
     return render(request, 'contact.html')
+    
