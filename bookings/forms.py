4ed668c33b6c0d1f1e05bcbed047804d9ee1d2ed
Maximiliane <maximiliane.kaempffer@gmail.com>
2024-04-11 07:28:30 +0000

    Refactor indentation of bookings form.py

--- bookings/forms.py
+++ bookings/forms.py
@@ -2,10 +2,17 @@ from django import forms
 from .models import Booking
 import datetime
 
+
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['guest_last_name', 'phone_number', 'date', 'time', 'num_people']
+        fields = [
+            'guest_last_name',
+            'phone_number',
+            'date',
+            'time',
+            'num_people'
+        ]
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'})
         }
